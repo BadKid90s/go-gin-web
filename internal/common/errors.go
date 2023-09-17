c7069c3e7ea68a8aac01022d76cc28074d8b99dc
wangruiyu <wry10150@163.com>
2023-09-17 08:22:28 +0800

    feature(error): 增加通用错误

---
+++ internal/common/errors.go
@@ -0,0 +1,18 @@
+package common
+
+// common errors
+
+var (
+	ErrSuccess             = newError(0, "ok")
+	ErrBadRequest          = newError(400, "Bad Request")
+	ErrUnauthorized        = newError(401, "Unauthorized")
+	ErrNotFound            = newError(404, "Not Found")
+	ErrInternalServerError = newError(500, "Internal Server Error")
+)
+
+// more biz errors
+
+var (
+	//
+	ErrUsernameAlreadyUsed = newError(1001, "The username is already in used.")
+)
