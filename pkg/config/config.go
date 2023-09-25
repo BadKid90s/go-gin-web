e05fcf703800acf665ed845ec1ada3a62dcc5d46
wangruiyu <wry10150@163.com>
2023-09-25 11:42:09 +0800

    feature(config): 修改配置环境

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,13 +1,20 @@
 package config
 
 import (
+	"fmt"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func NewConfig() *viper.Viper {
 	log.Println("loading conf file")
-	return getConfig("config/application.yml")
+	appEnv := os.Getenv("APP_ENV")
+	configFile := "application-prod.yml"
+	if appEnv == "" || appEnv == "dev" {
+		configFile = "application-dev.yml"
+	}
+	return getConfig(fmt.Sprintf("config/%s", configFile))
 }
 func getConfig(path string) *viper.Viper {
 	conf := viper.New()
