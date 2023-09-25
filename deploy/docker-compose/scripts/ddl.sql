667e43f30d6ee1b7467cac0224ee669350d1caf0
wangruiyu <wry10150@163.com>
2023-09-25 11:42:44 +0800

    feature(sql): 修改初始化脚本

--- deploy/docker-compose/scripts/ddl.sql
+++ deploy/docker-compose/scripts/ddl.sql
@@ -1,13 +1,11 @@
 CREATE DATABASE go_gin_web CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-
 USE go_gin_web;
-
 
 CREATE TABLE sys_tenants (
                            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
-                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
 
                            tenant_name VARCHAR(20) NOT NULL COMMENT '租户名称'
 
@@ -16,8 +14,8 @@ CREATE TABLE sys_tenants (
 
 CREATE TABLE sys_orgs (
                              id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
-                             created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                             updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                             created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                             updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
                              tenant_id INT UNSIGNED NOT NULL COMMENT '租户ID',
 
                              org_name VARCHAR(20) NOT NULL COMMENT '机构名称'
@@ -26,12 +24,12 @@ CREATE TABLE sys_orgs (
 
 CREATE TABLE sys_users (
                            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
-                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
 
                            user_name VARCHAR(20) NOT NULL COMMENT '用户名',
                            login_name VARCHAR(20) NOT NULL COMMENT '用户名',
-                           password VARCHAR(16) NOT NULL COMMENT '密码',
+                           password VARCHAR(64) NOT NULL COMMENT '密码',
                            mobile VARCHAR(11) NULL COMMENT '手机号码',
                            email VARCHAR(30) NULL COMMENT '邮箱',
                            enabled TINYINT(1) NOT NULL DEFAULT 1  COMMENT '用户状态 0-禁用，1-启用'
@@ -41,8 +39,8 @@ CREATE TABLE sys_users (
 
 CREATE TABLE sys_user_tenants (
                            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
-                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
 
                            user_id INT UNSIGNED NOT NULL COMMENT '机构ID',
                            tenant_id INT UNSIGNED NOT NULL COMMENT '租户Id'
@@ -51,8 +49,8 @@ CREATE TABLE sys_user_tenants (
 
 CREATE TABLE sys_user_orgs (
                                   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
-                                  created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                                  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                                  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                                  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
 
                                   user_id INT UNSIGNED NOT NULL COMMENT '机构ID',
                                   org_id INT UNSIGNED NOT NULL COMMENT '机构Id'
@@ -60,9 +58,9 @@ CREATE TABLE sys_user_orgs (
 
 
 CREATE TABLE sys_roles (
-                           id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',,
-                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                           id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
                            tenant_id INT UNSIGNED NOT NULL COMMENT '租户ID',
                            org_id INT UNSIGNED NOT NULL COMMENT '机构ID',
 
@@ -71,9 +69,9 @@ CREATE TABLE sys_roles (
 
 
 CREATE TABLE sys_user_roles (
-                           id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',,
-                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                           id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
                            tenant_id INT UNSIGNED NOT NULL COMMENT '租户ID',
                            org_id INT UNSIGNED NOT NULL COMMENT '机构ID',
 
@@ -84,9 +82,9 @@ CREATE TABLE sys_user_roles (
 
 
 CREATE TABLE sys_authorities  (
-                           id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',,
-                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                           id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+                           created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                           updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
                            tenant_id INT UNSIGNED NOT NULL COMMENT '租户ID',
                            org_id INT UNSIGNED NOT NULL COMMENT '机构ID',
 
@@ -95,9 +93,9 @@ CREATE TABLE sys_authorities  (
 
 
 CREATE TABLE sys_role_authorities  (
-                                  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',,
-                                  created_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '创建时间',
-                                  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIME()  COMMENT '更新时间',
+                                  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+                                  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+                                  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
                                   tenant_id INT UNSIGNED NOT NULL COMMENT '租户ID',
                                   org_id INT UNSIGNED NOT NULL COMMENT '机构ID',
 
